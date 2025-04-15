de17550e911dabb2f12c101aeb5e10d34b50ffd5
Avapage <85475589+Avapage@users.noreply.github.com>
2025-04-16 00:52:21 +0700

    Update and rename App.jsx to app.jsx

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import RSIHeatmap from './RSIHeatmap';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-900">
+      <RSIHeatmap />
+    </div>
+  );
+}
+
+export default App;
